47dcf17774c5a1900d24ae1c49efd6c07f92aef9
mohammedqanaam-art <mohammedqanaam@gmail.com>
2026-08-16 13:57:35 +0300

    feat: load redesigned admin workspace styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import AppErrorBoundary from "./components/AppErrorBoundary";
 import "./index.css";
 import "./styles/boudlExecutiveTheme.css";
 import "./styles/bhg2026.css";
+import "./styles/admin2026.css";
 
 createRoot(document.getElementById("root")!).render(
   <AppErrorBoundary>
